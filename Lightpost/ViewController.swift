d99790529a8a96e083883fdb8b7e08a5a931d944
lvlingsheng <lvlingsheng@gmail.com>
2016-02-13 15:37:52 -0600

    Initial Commit

---
+++ Lightpost/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lightpost
+//
+//  Created by 吕凌晟 on 16/2/13.
+//  Copyright © 2016年 Lingsheng Lyu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
